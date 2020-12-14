7f107de7636dd5d858ae0d0b3f2ee11f00ff6122
Andre Albuquerque <andre.t.albuquerque@gmail.com>
2020-12-14 16:16:57 -0300

    Alterar end-point

--- src/main/java/br/com/fiap/pacientes/controller/PacienteController.java
+++ src/main/java/br/com/fiap/pacientes/controller/PacienteController.java
@@ -12,7 +12,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pacientes")
+@RequestMapping("/api/pacientes")
 public class PacienteController {
 
     private final PacienteService pacienteService;
